ccd0e9413b134c30ba89b18d44259e0f67edf9b5
jangxx <janscheiper95@gmail.com>
2014-04-08 16:04:06 +0200

    Removed google analytics

--- page/index.php
+++ page/index.php
@@ -36,18 +36,6 @@ if ($_SESSION["SESS_ID"] == "") {
 		<script type="text/javascript" src="js/description.js" charset="ISO-8859-1"></script>
 		<script type="text/javascript" src="js/quotes.js" charset="ISO-8859-1"></script>
 		<?php if ($_SESSION["SESS_ADMIN"] == 1) echo '<script type="text/javascript" src="js/admin.js"></script>' . "\n"; ?>
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28790509-1']);
-		  _gaq.push(['_setDomainName', '.jangxx.com']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
 	</head>
 	<body onload="load()">
 		<div id="background"><canvas id="background-canvas"></canavs></div>
--- page/login.php
+++ page/login.php
@@ -22,18 +22,6 @@ if (!empty($_SESSION["SESS_ID"])) {
 		<script type="text/javascript" src="js/global.js"></script>
 		<script type="text/javascript" src="js/json2.js"></script>
 		<script type="text/javascript" src="js/login.js"></script>
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28790509-1']);
-		  _gaq.push(['_setDomainName', '.jangxx.com']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
 	</head>
 	<body onload="load()">
 		<div id="login-box">
